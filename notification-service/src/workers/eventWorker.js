1cc04b36d94c94af36cff1929f0c98e822181e2c
deabhi76 <deabhi76@gmail.com>
2026-07-24 20:43:28 +0000

    finish notification service before deploying

--- notification-service/src/workers/eventWorker.js
+++ notification-service/src/workers/eventWorker.js
@@ -10,18 +10,9 @@ const notificationService =
         '../services/notificationService'
     );
 
-const connection =
-    new Redis({
-
-        host:
-            process.env.REDIS_HOST,
-
-        port:
-            process.env.REDIS_PORT,
-
-        maxRetriesPerRequest:null
-
-    });
+const connection = new Redis(process.env.REDIS_URL, {
+    maxRetriesPerRequest: null,
+});
 
 const eventWorker =
     new Worker(
